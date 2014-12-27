b9ef4702a348b66345b7fdbfdc052de0a9bfe0f5
sunway <sunwayliving@gmail.com>
2014-12-27 13:25:21 +0800

    removed useless intermedia files in DepthSensing project

--- DepthSensing/Source/DX11BuildLinearSystem.cpp
+++ DepthSensing/Source/DX11BuildLinearSystem.cpp
@@ -100,10 +100,7 @@ HRESULT DX11BuildLinearSystem::OnD3D11CreateDevice( ID3D11Device* pd3dDevice )
 }
 
 HRESULT DX11BuildLinearSystem::applyBL( ID3D11DeviceContext* context, ID3D11ShaderResourceView* inputSRV, ID3D11ShaderResourceView* correspondenceSRV, ID3D11ShaderResourceView* correspondenceNormalsSRV, D3DXVECTOR3& mean, float meanStDev, 
-									   
-									   
-									   
-									   ::Matrix4f& deltaTransform, unsigned int imageWidth, unsigned int imageHeight, unsigned int level, Matrix6x7f& res, LinearSystemConfidence& conf )
+									   Eigen::Matrix4f& deltaTransform, unsigned int imageWidth, unsigned int imageHeight, unsigned int level, Matrix6x7f& res, LinearSystemConfidence& conf )
 {
 	HRESULT hr = S_OK;
 
